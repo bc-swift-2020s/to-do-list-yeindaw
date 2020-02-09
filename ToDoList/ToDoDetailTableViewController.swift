f19b7a8671c95bb341777e214630c58a180a2012
yeindaw <yedawin@gmail.com>
2020-02-09 15:41:22 -0500

    5.6

--- ToDoList/ToDoDetailTableViewController.swift
+++ ToDoList/ToDoDetailTableViewController.swift
@@ -13,16 +13,22 @@ class ToDoDetailTableViewController: UITableViewController {
     @IBOutlet weak var noteView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var nameField: UITextField!
-    var toDoItem: String!
+    var toDoItem: ToDoItem!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameField.text = toDoItem
+        if toDoItem == nil {
+            toDoItem = ToDoItem(name: "", date: Date(), notes: "")
+        }
+        nameField.text = toDoItem.name
+        datePicker.date = toDoItem.date
+        noteView.text = toDoItem.notes
+        
       
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-        toDoItem = nameField.text
+        toDoItem = ToDoItem(name: nameField.text!, date: datePicker.date, notes: noteView.text)
     }
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
         let isPresentingInAddMode = presentingViewController is UINavigationController
---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Dawin Ye on 2/9/20.
+//  Copyright © 2020 Dawin Ye. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
